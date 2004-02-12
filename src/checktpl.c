0d6d883361a2c10291c8d464bab14cf4d1fb7d83
Andrew Ladutsko <andrew_ladutsko@users.sourceforge.net>
2004-02-12 19:45:04 +0000

    small util to check templates for syntax errors

---
+++ src/checktpl.c
@@ -0,0 +1,32 @@
+/*
+ *  This program is used to check templates for syntax errors
+ *
+ */
+#include <stdio.h>
+#include "htpl.h"
+
+int main(int argc, char **argv)
+{
+    unsigned int i;
+
+    if (argc == 1)
+    {
+        printf("Check templates for syntax errors.\n");
+        printf("Usage: <template> [<template>...]\n");
+        return 1;
+    }
+
+    for (i = 1; i < argc; i++)
+    {
+        template *tpl = newTemplate();
+
+        if (parseTemplate(tpl, argv[i]))
+            printf("Template %s is OK\n", argv[i]);
+        else
+            printf("%s\n", htplError);
+
+        deleteTemplate(tpl);
+    }
+
+    return 0;
+}
